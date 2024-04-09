afc87c460cf789b21416c833ee16209f2e7e32d6
ohara takanari <takaboo07181994@gmail.com>
2024-04-10 01:55:39 +0900

    point_activities修正_2

--- app/helpers/point_activitys_helper.rb
+++
@@ -1,2 +0,0 @@
-module PointActivitysHelper
-end
--- test/controllers/point_activities_controller_test.rb
+++ test/controllers/point_activities_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class PointActivitysControllerTest < ActionDispatch::IntegrationTest
+class PointActivitiesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
