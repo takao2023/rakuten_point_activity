3649889622a95573421c069705efae3e3f8be73f
大原崇生 <ohara_takanari@ooharatseinoMBP.lan>
2026-04-12 23:51:39 +0900

    fix: add ensure_reminder_hours_column migration to guarantee DB schema consistency

---
+++ db/migrate/20260412150000_ensure_reminder_hours_column.rb
@@ -0,0 +1,7 @@
+class EnsureReminderHoursColumn < ActiveRecord::Migration[7.1]
+  def change
+    unless column_exists?(:notification_settings, :reminder_hours)
+      add_column :notification_settings, :reminder_hours, :text, default: "[]"
+    end
+  end
+end
