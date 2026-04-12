27040a0816d2bbac6e052b4aed100bdcc60925f8
大原崇生 <ohara_takanari@ooharatseinoMBP.lan>
2026-04-13 00:15:08 +0900

    fix: remove illegal default value from TEXT column migration for MySQL compatibility

--- db/migrate/20260412150000_ensure_reminder_hours_column.rb
+++ db/migrate/20260412150000_ensure_reminder_hours_column.rb
@@ -1,7 +1,7 @@
 class EnsureReminderHoursColumn < ActiveRecord::Migration[7.1]
   def change
     unless column_exists?(:notification_settings, :reminder_hours)
-      add_column :notification_settings, :reminder_hours, :text, default: "[]"
+      add_column :notification_settings, :reminder_hours, :text
     end
   end
 end
