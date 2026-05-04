bce6a191832beb777fb3998298f121c27f505c8a
大原崇生 <ohara_takanari@ooharatakashiseinoMacBook-Pro.local>
2026-05-04 20:25:48 +0900

    fix: allow Cloud Run hosts in development config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Cloud Runでのアクセス許可
+  config.hosts.clear
 end
